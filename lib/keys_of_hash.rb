106f41b99356564788c1874b09c5aaabbf1bfdaf
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-07-15 05:24:32 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,11 @@
 class Hash
   def keys_of(*arguments)
 		animals = []
-		self.each { |animal, place|
+		self.each do |animal, place|
 			if arguments.include?(place)
 				animals.push(animal)
 			end
+		end
 		}
 		return animals
   end
